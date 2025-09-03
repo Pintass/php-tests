5cd70fb50650229ce5716d86d416097a9810b61a
Daniel Rodrigues Amorim <105808962+Pintass@users.noreply.github.com>
2025-09-03 12:52:36 +0200

    Update page1.php

--- TPWeb/page1.php
+++ TPWeb/page1.php
@@ -21,6 +21,22 @@
 <h1> Damier 10x10 </h1>
 <table>
 <?php
+
+echo "<form action='' method='post'>
+<label for='ligne'>Ligne</label>
+<input type='text' name='ligne' id='ligne'>
+<label for='col'>Colonne</label>
+<input type='text' name='col' id='col' id='col'>
+<input type='submit' value='Valider'>
+</form>";
+
+if (isset($_POST['ligne'], $_POST['col'])) {
+    $ligne = $_POST['ligne'];
+    $col = $_POST['col'];
+    echo $ligne."<br>".$col;
+}
+
+
 for ($x = 0; $x <= 10; $x++) {
     echo"<tr>";
     for ($i = 1; $i <= 5; $i++) {
